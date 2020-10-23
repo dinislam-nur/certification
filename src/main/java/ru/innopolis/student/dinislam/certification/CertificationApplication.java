f4d9139df350680d3b89da0cbba2a730811cdbd6
dinislam-nur <dinislamdamn@gmail.com>
2020-10-23 10:03:13 +0300

    Initial commit

---
+++ src/main/java/ru/innopolis/student/dinislam/certification/CertificationApplication.java
@@ -0,0 +1,13 @@
+package ru.innopolis.student.dinislam.certification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CertificationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CertificationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/innopolis/student/dinislam/certification/CertificationApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.innopolis.student.dinislam.certification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CertificationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
